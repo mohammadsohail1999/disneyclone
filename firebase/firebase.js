8ee399dd4018a62560f99ecb00cb91acffc7d912
Mohammad Sohail <mohammad.sohail@appinventiv.com>
2022-09-23 00:16:55 +0530

    firebase issue fixed

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
